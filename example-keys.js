91596441f63a6bd96e2559304eb1948514257c52
James Benze <benzejaa@gmail.com>
2012-02-10 16:23:37 -0500

    added example keys file

---
+++ example-keys.js
@@ -0,0 +1,18 @@
+/******************************************************************************\
+*                                                                              *
+*  THIS IS AN EXAMPLE KEYS FILE.                                               *
+*  In order to get the bot to work, please rename this file to "keys.js" and   *
+*  fill it out as appropriate to your system.                                  *
+*                                                                              *
+\******************************************************************************/
+
+module.exports = {
+   AUTH: 'auth+live+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+   USERID: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+   ROOMID: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+
+   //Note:  This MUST be an absolute path!
+   DATABASE_LOC: "/xxx",
+
+   NODE_LOC: "/xxx"
+};
